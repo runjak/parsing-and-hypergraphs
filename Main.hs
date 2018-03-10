ba70dba22e2debcba78c0c23184cb3c7b2c7ed90
Nathalie Bargenda <n.bargenda@gmail.com>
2018-03-10 14:02:09 +0100

    My main is Forgetfulness

--- Main.hs
+++ Main.hs
@@ -1,36 +1,10 @@
 module Main where
 
+import Types
+import PosTagger (combinedTagger)
+
+
 main = putStrLn "TEST"
 
 sampleSentence :: String
 sampleSentence = "Donald beoabachtet Daisy mit dem Fernglas"
-
-type Span = (Int, Int)
-
-data Category = NP | V | N | P | Art | Unknown
-  deriving (Show, Eq, Ord)
-
-tag :: String -> [Category]
-tag = map tag' . words
-  where
-    tag' :: String -> Category
-    tag' "Donald"      = NP
-    tag' "beoabachtet" = V
-    tag' "Daisy"       = NP
-    tag' "mit"         = P
-    tag' "dem"         = Art
-    tag' "Fernglas"    = N
-    tag' _             = Unknown
-
-spanify :: String -> [Span]
-spanify = go 0 . words
-  where
-    go :: Int -> [String] -> [Span]
-    go i (w:ws) =
-      let l = length w
-          i' = i + l
-      in (i, i') : go i' ws
-    go _ [] = []
-
-combinedTagger :: String -> [(Category, Span)]
-combinedTagger sentence = zip (tag sentence) (spanify sentence)
---
+++ PosTagger.hs
@@ -0,0 +1,29 @@
+module PosTagger where
+
+import Types
+
+
+    tag :: String -> [Category]
+    tag = map tag' . words
+        where
+            tag' :: String -> Category
+            tag' "Donald"      = NP
+            tag' "beoabachtet" = V
+            tag' "Daisy"       = NP
+            tag' "mit"         = P
+            tag' "dem"         = Art
+            tag' "Fernglas"    = N
+            tag' _             = Unknown
+
+    spanify :: String -> [Span]
+    spanify = go 0 . words
+        where
+            go :: Int -> [String] -> [Span]
+            go i (w:ws) =
+                let l = length w
+                    i' = i + l
+                in (i, i') : go i' ws
+            go _ [] = []
+
+    combinedTagger :: String -> [(Category, Span)]
+    combinedTagger sentence = zip (tag sentence) (spanify sentence)
