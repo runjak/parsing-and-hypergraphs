61ab4b8f7f55bdbd843f7568b53efede7a882e50
Jakob Runge <sicarius@g4t3.de>
2018-03-10 15:22:19 +0100

    Complete applyRule

--- ActiveRules.hs
+++ ActiveRules.hs
@@ -1,5 +1,6 @@
 module ActiveRules where
 
+import qualified Data.Maybe as Maybe
 import qualified Data.List as List
 import Control.Monad
 import Types
@@ -26,10 +27,28 @@ combineSpans edges = let headSpan = getSpan $ head edges
                          lastSpan = getSpan $ last edges
                      in Just (fst headSpan, snd lastSpan)
 
-{-applyRule :: ProductionRule -> [Edge] -> [[Edge]]
-applyRule (ruleHead,ruleBody) edges = do
+applyRule :: ProductionRule -> [Edge] -> [[Edge]]
+applyRule _ [] = []
+applyRule rule@(ruleHead, ruleBody) edges = do
     (prefix, suffix) <- zip (List.inits edges) (List.tails edges)
     let suffixCategories = map getCategory suffix
     guard $ List.isPrefixOf ruleBody suffixCategories
+    let (replacedEdges, suffixSuffix) = List.splitAt (length ruleBody) suffix
+        newSpan' = combineSpans replacedEdges
+    guard $ Maybe.isJust newSpan'
+    let newEdge = ActiveEdge rule (Maybe.fromJust newSpan') replacedEdges
+    return $ prefix ++ [newEdge] ++ suffixSuffix
 
-applyRule _ [] = []-}
+testApplyRule = applyRule testRule testEdges
+  where
+    testRule = activeRules !! 1
+
+    testEdges :: [Edge]
+    testEdges = [ PassiveEdge N (0, 4)
+                , PassiveEdge S (5, 6)
+                , PassiveEdge PP (7, 10)
+                , PassiveEdge N (11, 13)
+                , PassiveEdge S (14, 16)
+                , PassiveEdge PP (17, 20)
+                , PassiveEdge N (21, 13)
+                ]
